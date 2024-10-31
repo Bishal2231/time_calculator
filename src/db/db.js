a277de62f56c7cd011da9fdbac7b3e025319cb3d
bishal ad <144100863+Bishal2231@users.noreply.github.com>
2024-10-31 12:15:55 +0545

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -6,7 +6,7 @@ dotenv.config()
 const DBCONNECT= async()=>{
 try {
     
-    const connection =await mongoose.connect(`${process.env.DB_URL}/aatish`)
+    const connection =await mongoose.connect(`${process.env.DB_URL}/wageAnalyzer`)
 } catch (error) {
     throw new Error("error at db connection ")
     
